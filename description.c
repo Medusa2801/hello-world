79e2a2e6bb3830b8d252e5470e2f840041e6565c
Medusa <mateus.l.silveira@gmail.com>
2020-05-02 17:28:53 -0300

    update of the description

--- description.c
+++ description.c
@@ -2,5 +2,8 @@
 
 main()
 {
-  printf("Hello World!\n");
+    printf("Oi, mores. Aqui quem fala é o Dofono de Obaluaê, filho de Rita de Onirá, filha de Jesonita de Exú.\n");
+    printf("Tenho 8 meses de santo e amo minha religião. Louvo àqueles que de mim cuidam: Obaluaê  e  Iansã  -\n");
+    printf("meus guias nessa vida -, minha mãe de santo, a mãe ekedy, minha ajibonan, meu padrinho de vela. Que\n");
+    printf("sejam felizes e realizados em seus caminhos. Asé.\n");
 }
